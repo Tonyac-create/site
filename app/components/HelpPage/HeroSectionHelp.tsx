9cd6178d55f760cc70fa30aec3624db975020003
Angelique <angelique.copere@gmail.com>
2025-04-23 16:49:49 +0200

    add video

--- app/components/HelpPage/HeroSectionHelp.tsx
+++ app/components/HelpPage/HeroSectionHelp.tsx
@@ -8,7 +8,11 @@ export default function HeroSectionHelp() {
                 <p className="text-xl mt-3 mb-5 w-[80%]">Chaque jour, des milliers d’animaux sont abandonnés, maltraités ou en danger.
                      Grâce à votre générosité, nous pouvons les secourir, les soigner et leur offrir une seconde chance.
                     100% des dons et adhésions servent à financer nos actions de sauvetage, de soins et d’adoption.
-                    Chaque euro compte : même un petit don fait une grande différence !​</p>
+                    Chaque euro compte : même un petit don fait une grande différence !</p>
+            </div>
+            <div className="flex flex-col justify-center lg:flex-row gap-11">
+                <video src="/videos/pub.mp4" controls className="w-[500px] h-[300px]"></video>
+                <video src="/videos/Renard.mp4" controls className="w-[500px] h-[300px]"></video>
             </div>
         </section>
     );
