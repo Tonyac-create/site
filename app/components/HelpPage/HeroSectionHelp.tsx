3c6b74e8e7cc6e14b7ae28ee7145d371a7e4e76a
Angelique <angelique.copere@gmail.com>
2025-05-27 14:11:19 +0200

    update spacing videos page help

--- app/components/HelpPage/HeroSectionHelp.tsx
+++ app/components/HelpPage/HeroSectionHelp.tsx
@@ -10,7 +10,7 @@ export default function HeroSectionHelp() {
                     100% des dons et adhésions servent à financer nos actions de sauvetage, de soins et d’adoption.
                     Chaque euro compte : même un petit don fait une grande différence !</p>
             </div>
-            <div className="flex flex-col justify-center lg:flex-row gap-11">
+            <div className="flex flex-col justify-center pt-5 ml-6 lg:flex-row gap-11">
                 <video src="/videos/pub.mp4" controls className="w-[500px] h-[300px]"></video>
                 <video src="/videos/Renard.mp4" controls className="w-[500px] h-[300px]"></video>
             </div>
