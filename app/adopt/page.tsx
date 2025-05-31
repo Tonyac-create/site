d57ff897dff3b8501c0e71245208137583948894
Angelique <angelique.copere@gmail.com>
2025-05-31 16:04:22 +0200

    update

--- app/adopt/page.tsx
+++ app/adopt/page.tsx
@@ -1,12 +1,15 @@
 import Adopt from "../components/AdoptPage/AdoptAnimalSection";
 import Adopted from "../components/AdoptPage/AdoptedAnimalSection";
 import HeroSectionAdopt from "../components/AdoptPage/HeroSectionAdopt";
+import { Suspense } from 'react';
 
 export default function AdoptPage() {
     return (
         <main>
             <HeroSectionAdopt />
-            <Adopt />
+            <Suspense fallback={<div>Chargement...</div>}>
+                <Adopt />
+            </Suspense>
             <Adopted />
         </main>
     )
