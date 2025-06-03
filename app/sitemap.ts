f68ffe693ec30c1105dd41a9ed4bd95fa7b4752a
Angelique <angelique.copere@gmail.com>
2025-06-03 17:12:16 +0200

    add robots et sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,72 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: 'https://sfpa-71.fr',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+        {
+            url: 'https://sfpa-71.fr/adopt',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://sfpa-71.fr/blog',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: 'https://sfpa-71.fr/contact',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: 'https://sfpa-71.fr/missions',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: 'https://sfpa-71.fr/mentions',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: 'https://sfpa-71.fr/politics',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: 'https://sfpa-71.fr/team',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: 'https://sfpa-71.fr/visits',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: 'https://sfpa-71.fr/history',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: 'https://sfpa-71.fr/help',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        }
+    ]
+}
