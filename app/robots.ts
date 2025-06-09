db5ac2b234e0c021b6ae0fa7770e6624bb26f0df
Angelique <angelique.copere@gmail.com>
2025-06-09 17:04:39 +0200

    update url robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://sfpa-71.fr/sitemap.xml',
+    sitemap: 'https://sfpa71.fr/sitemap.xml',
   }
 }
